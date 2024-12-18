0b3ee78e3b0bd65698414e5c14900c14384c0ff2
Masakazu Shirai <attakadou@gmail.com>
2024-12-18 18:58:07 +0900

    Initial commit

---
+++ api/reviews.js
@@ -0,0 +1,21 @@
+﻿const axios = require('axios');
+
+module.exports = async (req, res) => {
+    const API_KEY = 'AIzaSyB5bNeXwCJaBfTbiM4wVdZiso3Escxn4-4'; // Google APIキーをここに入力
+    const PLACE_ID = 'ChIJb8LG9Z_fVDURCYmh_TeN2To'; // 対象のGoogleプレイスIDをここに入力
+
+    try {
+        const response = await axios.get('https://maps.googleapis.com/maps/api/place/details/json', {
+            params: {
+                place_id: PLACE_ID,
+                fields: 'reviews',
+                key: API_KEY
+            }
+        });
+        const reviews = response.data.result.reviews;
+        res.status(200).json(reviews);
+    } catch (error) {
+        console.error('Error fetching reviews:', error);
+        res.status(500).json({ error: 'Failed to fetch reviews' });
+    }
+};
